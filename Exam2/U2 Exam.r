2c89f9d9d9a9990af491b5afea9fcbd77ae724bb
BrayanBaltazar <71296780+BrayanBaltazar@users.noreply.github.com>
2022-05-04 16:40:40 -0700

    update exam 2

--- Exam2/U2 Exam.r
+++ Exam2/U2 Exam.r
@@ -8,37 +8,41 @@ setwd("C:/Users/braya/Desktop/Unit_2")
 
 getwd()
 
-movies <- read.csv("Project-Data.csv")
+Pelicula <- read.csv("Project-Data.csv")
 
-movies <- movies [c(3,6,8,18)]
-
-
-
-
-movies<-movies[
-movies$Genre=="action"|
-movies$Genre=="adventure"|
-movies$Genre=="animation"|movies$Genre=="comedy"|
-movies$Genre=="drama",]
-
-movies<-movies[
-movies$Studio=="Buena Vista Studios"|
-movies$Studio=="Fox"| movies$Studio=="Paramount Pictures"|
-movies$Studio=="Sony"|movies$Studio=="Universal" |
-movies$Studio=="WB",]
-
-colnames(movies) <- c("Genre", "Studio", "BudgetInMillions", "GrossUS")
+Pelicula <- Pelicula [c(3,6,8,17)]
 
 
 
-str(movies)
 
-summary(movies)
+Pelicula<-Pelicula[
+Pelicula$Genre=="action"|
+Pelicula$Genre=="adventure"|
+Pelicula$Genre=="animation"|
+Pelicula$Genre=="comedy"|
+Pelicula$Genre=="drama",]
 
-BoxPlot <- ggplot(movies, aes(x=Genre, y=GrossUS, color=Studio, size=BudgetInMillions))
+Pelicula<-Pelicula[
+Pelicula$Studio=="Buena Vista Studios"|
+Pelicula$Studio=="Fox"| 
+Pelicula$Studio=="Paramount Pictures"|
+Pelicula$Studio=="Sony"|
+Pelicula$Studio=="Universal" |
+Pelicula$Studio=="WB",]
+
+colnames(Pelicula) <- c("Genre", "Studio", "BudgetIn $ M", "GrossUS")
+
+
+
+str(Pelicula)
+
+summary(Pelicula)
+
+BoxPlot <- ggplot(Pelicula, aes(x=Genre, y=GrossUS, color=Studio, size=BudgetInMillions))
 
 BoxPlot + geom_boxplot()
 
 BoxPlot + geom_jitter(shape=20) + geom_boxplot(size=0.5,alpha=0.8,color="Black",
-outlier.shape = NA)+ theme(plot.title = element_text(color="Black", size=16, face="bold", hjust = 0.5),
-axis.title.x = element_text(color="Purple", size=16), axis.title.y = element_text(color="Purple", size=16))+  ggtitle("Domestic Gross % by Genre") + ylab("GrossUS") + xlab("Genre")
+outlier.shape = NA)+ theme(plot.title = element_text(color="Black",size=16, face="bold", hjust = 0.5),
+axis.title.x = element_text(color="Purple", size=16), axis.title.y = element_text(color="Purple", size=16))
++  ggtitle("Domestic Gross % by Genre") + ylab("GrossUS") + xlab("Genre")
