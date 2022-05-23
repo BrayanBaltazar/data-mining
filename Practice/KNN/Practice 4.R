a580f213bc53716edb19a0ec0e2d84bbdfa508dd
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2022-05-23 14:05:50 -0700

    Practice 4 Adding K-NN

--- Practice/KNN/Practice 4.R
+++ Practice/KNN/Practice 4.R
@@ -39,7 +39,7 @@ y_pred = knn(train = training_set[, -3],
 
 cm = table(test_set[, 3], y_pred)
 
-# Visualising the Training set results
+# Visualize the Training set results
 
 library(ElemStatLearn)
 set = training_set
@@ -56,7 +56,7 @@ contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Visualising the Test set results
+# Visualize the Test set results
 
 library(ElemStatLearn)
 set = test_set
